63bab6a7aa73a2f906ff8bb505e1bcadd38cb223
pankajgehlot <pankajgehlot30@gmail.com>
2016-12-03 11:26:27 -0500

    testing 2

--- app/src/main/java/com/example/bucketdrop/pankajpc/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/example/bucketdrop/pankajpc/bucketdrops/ActivityMain.java
@@ -10,6 +10,6 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("testing","testing program");
+        Log.i("testing2","testing program");
     }
 }
